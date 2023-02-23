70e04dc123bea1f6ef882586b148a4ab7bd03521
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-02-23 18:42:45 -0500

    fix urls

--- greetings/urls.py
+++ greetings/urls.py
@@ -2,10 +2,11 @@
 URLs for greetings.
 """
 from django.urls import path  # pylint: disable=unused-import
+from rest_framework.routers import DefaultRouter
 
 
 from .views import GreetingsViewSet
 
-urlpatterns = [
-    path('', GreetingsViewSet.as_view(), name='greetings'),
-]
+router = DefaultRouter()
+router.register(r'', GreetingsViewSet, basename='greetings')
+urlpatterns = router.urls
