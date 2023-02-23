2b6d5d725ede3948b4f7a262dbbf3397b810748c
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-02-23 13:46:17 -0500

    ad urls

--- greetings/apps.py
+++ greetings/apps.py
@@ -3,6 +3,8 @@ greetings Django application initialization.
 """
 
 from django.apps import AppConfig
+from edx_django_utils.plugins import PluginURLs
+from openedx.core.djangoapps.plugins.constants import ProjectType
 
 
 class GreetingsConfig(AppConfig):
@@ -11,3 +13,18 @@ class GreetingsConfig(AppConfig):
     """
 
     name = 'greetings'
+    plugin_app = {
+        PluginURLs.CONFIG: {
+            # The three dict attributes literally equate to the following
+            # lines of code being injected into edx-platform/lms/urls.py:
+            #
+            # import myapp.urls.py
+            # url(r"^my-app/", include((urls, "my_app"), namespace="my_app")),
+            #
+            ProjectType.LMS: {
+                PluginURLs.NAMESPACE: name,
+                PluginURLs.REGEX: "^greetings/",
+                PluginURLs.RELATIVE_PATH: "urls",
+            }
+        }
+    }
